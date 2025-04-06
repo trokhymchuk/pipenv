b2b84c1a343cfa475d1d0875174a373ec01b514e
Artem Trokhymchuk <trkhmchk@gmail.com>
2025-04-06 16:00:36 +0300

    add test_input_pandas_no_extension

--- tests/test_io/test_input.py
+++ tests/test_io/test_input.py
@@ -12,3 +12,23 @@ def test_input_builtin():
         result = input_builtin(tmp_file.name)
 
     assert result == test_content
+
+
+def test_input_pandas_no_extension():
+    expected_pandas_dataframe = pandas.DataFrame({
+        "a": ["1", "artem"],
+        "b": ["2", "trokhymchuk"],
+        "c": ["3", "artem"],
+    })
+
+    test_content = expected_pandas_dataframe.to_csv(index=False)
+
+    with tempfile.NamedTemporaryFile(mode='w+', encoding='utf-8', delete=True) as tmp_file:
+        tmp_file.write(test_content)
+        tmp_file.flush()
+        result = input_pandas(tmp_file.name)
+
+    # print(expected_pandas_dataframe)
+    # print("===")
+    # print(result)
+    pandas.testing.assert_frame_equal(result, expected_pandas_dataframe)
