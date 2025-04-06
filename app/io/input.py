45e125550a8b12f2024639909443f9799c0b097b
Artem Trokhymchuk <trkhmchk@gmail.com>
2025-04-06 15:03:36 +0300

    add function stubs

---
+++ app/io/input.py
@@ -0,0 +1,30 @@
+def input_console():
+    """
+    Return input from user from console.
+    Return:
+        str. text, entered by the user.
+    """
+    pass
+
+
+def input_builtin(file_path):
+    """
+    Return contents of the file using builtin functions.
+    Args:
+        file_path(str): path to the file to be read.
+    Return:
+        str: contents of the file.
+    """
+    pass
+
+
+def input_pandas(file_path):
+    """
+    Return contents of the file, parsed, using pandsas.
+    Args:
+        file_path(str): The path to the file to be read.
+
+    Returns:
+        df(pandas.DataFrame): contents of the file, parsed.
+    """
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,17 @@
+def output_console(text):
+    """
+    Print text to the console.
+    Args:
+        text (str): text to display to print in console.
+    """
+    pass
+
+
+def output_builtin(text, file_path):
+    """
+    Print text to file using built-in functions.
+    Args:
+        text (str): text to display to print in console.
+        file_path(str): The path to the file to output to.
+    """
+    pass
